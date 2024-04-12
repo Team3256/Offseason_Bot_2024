6d6b0c8161c08df37dde5bed9e324b0cfe38c7f0
Ryan Abraham <ryan.ryanabraham@gmail.com>
2024-04-12 16:05:33 -0700

    Add InterpolatingDoubleTreeMap to PivotShooter.java Refactor speakerId variables in PivotShooterAutoAimPredict.java and PivotShooterAutoAimSidEdition.java

--- src/main/java/frc/robot/subsystems/pivotshooter/PivotShooter.java
+++ src/main/java/frc/robot/subsystems/pivotshooter/PivotShooter.java
@@ -8,11 +8,17 @@
 package frc.robot.subsystems.pivotshooter;
 
 import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.wpilibj.RobotBase;
 import frc.robot.Constants;
 import frc.robot.utils.SinglePositionSubsystem;
 
 public class PivotShooter extends SinglePositionSubsystem {
+  public InterpolatingDoubleTreeMap pivotMotorData = new InterpolatingDoubleTreeMap();
+  public InterpolatingDoubleTreeMap pivotMotorDataNotGlobalPose = new InterpolatingDoubleTreeMap();
+  public InterpolatingDoubleTreeMap pivotMotorDataNotGlobalPose2 =
+      new InterpolatingDoubleTreeMap(); // for second april
 
   public PivotShooter() {
     super(PivotShooterConstants.kUseMotionMagic, PivotShooterConstants.kCurrentThreshold, PivotShooterConstants.kStallVelocityThreshold);
@@ -20,6 +26,25 @@ public class PivotShooter extends SinglePositionSubsystem {
     if(!RobotBase.isReal()) {
       super.configureSimHardware(PivotShooterConstants.kNumPivotMotors, PivotShooterConstants.kPivotMotorGearing, PivotShooterConstants.jKgMetersSquared,PivotShooterConstants.kPivotLength, PivotShooterConstants.kPivotMinAngleDeg, PivotShooterConstants.kPivotMaxAngleDeg, false);
     }
+    setupPivotShooterData();
+  }
+
+  private void setupPivotShooterData() {
+
+    // pivotMotorData.put(0.051, kSubWooferPreset);
+    // pivotMotorData.put(1.4, 6.25 / 138.333);
+    // pivotMotorData.put(0.945, 5.8 / 138.333);
+    // pivotMotorData.put(1.69, 6.5 / 138.333);
+    // pivotMotorData.put(0.485, 5.25 / 138.333);
+    pivotMotorData.put(19.771, PivotShooterConstants.kSubWooferPreset);
+    // pivotMotorData.put(3.19, 5.6/138.333); // DO NOT use to interpolate for now
+    // distance to speaker and then angle
+  }
+
+
+  private void setupPivotShooterDataNotGlobalPose() {
+    pivotMotorDataNotGlobalPose.put(69.2, 2.1); // distance to speaker and then angle
+    pivotMotorDataNotGlobalPose2.put(69.1, 69.2); // distance to speaker and then angle
   }
 
   @Override
