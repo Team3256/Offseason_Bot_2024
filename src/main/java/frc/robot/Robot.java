f933fd04342c1de13dd8beb28efbc0ff798deda7
Ryan Abraham <ryan.ryanabraham@gmail.com>
2024-04-14 20:23:16 -0700

    warmup for autos

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -20,6 +20,8 @@ import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.NT4Publisher;
 import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
+import com.pathplanner.lib.commands.FollowPathCommand;
+
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -123,6 +125,7 @@ public class Robot extends LoggedRobot {
     if (Constants.kEnableAdvKit) {
       Logger.start();
     }
+    FollowPathCommand.warmupCommand().schedule();
 
     //    FollowPathCommand warmupCommand = new FollowPathCommand();
     //    warmupCommand.schedule();
