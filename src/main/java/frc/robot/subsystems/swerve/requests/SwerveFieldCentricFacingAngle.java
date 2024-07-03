4a9a1c408dbfbf4d52177e6f2088179f46c165aa
Ryan Abraham <ryan.ryanabraham@gmail.com>
2024-07-02 20:11:14 -0700

    🎨 Made the custom swerve request subclass instead

--- src/main/java/frc/robot/subsystems/swerve/requests/SwerveFieldCentricFacingAngle.java
+++ src/main/java/frc/robot/subsystems/swerve/requests/SwerveFieldCentricFacingAngle.java
@@ -15,95 +15,8 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 
-public class SwerveFieldCentricFacingAngle implements SwerveRequest {
-  /**
-   * The velocity in the X direction, in m/s. X is defined as forward according to WPILib
-   * convention, so this determines how fast to travel forward.
-   */
-  public double VelocityX = 0;
-  /**
-   * The velocity in the Y direction, in m/s. Y is defined as to the left according to WPILib
-   * convention, so this determines how fast to travel to the left.
-   */
-  public double VelocityY = 0;
-  /**
-   * The desired direction to face. 0 Degrees is defined as in the direction of the X axis. As a
-   * result, a TargetDirection of 90 degrees will point along the Y axis, or to the left.
-   */
-  public Rotation2d TargetDirection = new Rotation2d();
+public class SwerveFieldCentricFacingAngle extends SwerveRequest.FieldCentricFacingAngle implements SwerveRequest {
 
-  /** The allowable deadband of the request. */
-  public double Deadband = 0;
-  /** The rotational deadband of the request. */
-  public double RotationalDeadband = 0;
-  /**
-   * The center of rotation the robot should rotate around. This is (0,0) by default, which will
-   * rotate around the center of the robot.
-   */
-  public Translation2d CenterOfRotation = new Translation2d();
-
-  /** The type of control request to use for the drive motor. */
-  public SwerveModule.DriveRequestType DriveRequestType =
-      SwerveModule.DriveRequestType.OpenLoopVoltage;
-  /** The type of control request to use for the steer motor. */
-  public SwerveModule.SteerRequestType SteerRequestType = SwerveModule.SteerRequestType.MotionMagic;
-
-  /**
-   * The PID controller used to maintain the desired heading. Users can specify the PID gains to
-   * change how aggressively to maintain heading.
-   *
-   * <p>This PID controller operates on heading radians and outputs a target rotational rate in
-   * radians per second.
-   */
-  public PhoenixPIDController HeadingController = new PhoenixPIDController(0, 0, 0);
-
-  /** The perspective to use when determining which direction is forward. */
-  public ForwardReference ForwardReference = SwerveRequest.ForwardReference.OperatorPerspective;
-
-  public StatusCode apply(
-      SwerveControlRequestParameters parameters, SwerveModule... modulesToApply) {
-    double toApplyX = VelocityX;
-    double toApplyY = VelocityY;
-    Rotation2d angleToFace = TargetDirection;
-    if (ForwardReference == SwerveRequest.ForwardReference.OperatorPerspective) {
-      /* If we're operator perspective, modify the X/Y translation by the angle */
-      Translation2d tmp = new Translation2d(toApplyX, toApplyY);
-      tmp = tmp.rotateBy(parameters.operatorForwardDirection);
-      toApplyX = tmp.getX();
-      toApplyY = tmp.getY();
-      /* And rotate the direction we want to face by the angle */
-      angleToFace = angleToFace.rotateBy(parameters.operatorForwardDirection);
-    }
-
-    double rotationRate =
-        HeadingController.calculate(
-            parameters.currentPose.getRotation().getRadians(),
-            angleToFace.getRadians(),
-            parameters.timestamp);
-
-    double toApplyOmega = rotationRate;
-    if (Math.sqrt(toApplyX * toApplyX + toApplyY * toApplyY) < Deadband) {
-      toApplyX = 0;
-      toApplyY = 0;
-    }
-    if (Math.abs(toApplyOmega) < RotationalDeadband) {
-      toApplyOmega = 0;
-    }
-
-    ChassisSpeeds speeds =
-        ChassisSpeeds.discretize(
-            ChassisSpeeds.fromFieldRelativeSpeeds(
-                toApplyX, toApplyY, toApplyOmega, parameters.currentPose.getRotation()),
-            parameters.updatePeriod);
-
-    var states = parameters.kinematics.toSwerveModuleStates(speeds, CenterOfRotation);
-
-    for (int i = 0; i < modulesToApply.length; ++i) {
-      modulesToApply[i].apply(states[i], DriveRequestType, SteerRequestType);
-    }
-
-    return StatusCode.OK;
-  }
 
   /**
    * Sets the velocity in the X direction, in m/s. X is defined as forward according to WPILib
@@ -112,8 +25,9 @@ public class SwerveFieldCentricFacingAngle implements SwerveRequest {
    * @param velocityX Velocity in the X direction, in m/s
    * @return this request
    */
+  @Override
   public SwerveFieldCentricFacingAngle withVelocityX(double velocityX) {
-    this.VelocityX = velocityX;
+    super.VelocityX = velocityX;
     return this;
   }
 
@@ -124,8 +38,9 @@ public class SwerveFieldCentricFacingAngle implements SwerveRequest {
    * @param velocityY Velocity in the Y direction, in m/s
    * @return this request
    */
+  @Override
   public SwerveFieldCentricFacingAngle withVelocityY(double velocityY) {
-    this.VelocityY = velocityY;
+    super.VelocityY = velocityY;
     return this;
   }
 
@@ -136,8 +51,9 @@ public class SwerveFieldCentricFacingAngle implements SwerveRequest {
    * @param targetDirection Desired direction to face
    * @return this request
    */
+  @Override
   public SwerveFieldCentricFacingAngle withTargetDirection(Rotation2d targetDirection) {
-    this.TargetDirection = targetDirection;
+    super.TargetDirection = targetDirection;
     return this;
   }
 
@@ -148,7 +64,7 @@ public class SwerveFieldCentricFacingAngle implements SwerveRequest {
    * @return this request
    */
   public SwerveFieldCentricFacingAngle withDeadband(double deadband) {
-    this.Deadband = deadband;
+    super.Deadband = deadband;
     return this;
   }
 
@@ -159,7 +75,7 @@ public class SwerveFieldCentricFacingAngle implements SwerveRequest {
    * @return this request
    */
   public SwerveFieldCentricFacingAngle withRotationalDeadband(double rotationalDeadband) {
-    this.RotationalDeadband = rotationalDeadband;
+    super.RotationalDeadband = rotationalDeadband;
     return this;
   }
 
@@ -170,7 +86,7 @@ public class SwerveFieldCentricFacingAngle implements SwerveRequest {
    * @return this request
    */
   public SwerveFieldCentricFacingAngle withCenterOfRotation(Translation2d centerOfRotation) {
-    this.CenterOfRotation = centerOfRotation;
+    super.CenterOfRotation = centerOfRotation;
     return this;
   }
 
@@ -182,7 +98,7 @@ public class SwerveFieldCentricFacingAngle implements SwerveRequest {
    */
   public SwerveFieldCentricFacingAngle withDriveRequestType(
       SwerveModule.DriveRequestType driveRequestType) {
-    this.DriveRequestType = driveRequestType;
+    super.DriveRequestType = driveRequestType;
     return this;
   }
 
@@ -194,13 +110,13 @@ public class SwerveFieldCentricFacingAngle implements SwerveRequest {
    */
   public SwerveFieldCentricFacingAngle withSteerRequestType(
       SwerveModule.SteerRequestType steerRequestType) {
-    this.SteerRequestType = steerRequestType;
+    super.SteerRequestType = steerRequestType;
     return this;
   }
 
   public SwerveFieldCentricFacingAngle withHeadingController(
       PhoenixPIDController headingController) {
-    this.HeadingController = headingController;
+    super.HeadingController = headingController;
     return this;
   }
 }
