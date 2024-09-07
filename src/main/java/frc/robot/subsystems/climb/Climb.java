13584ea344aafdddd679eb752bac3246fdc60e5c
ThatXliner <bryan.hu.2020@gmail.com>
2024-09-06 17:20:44 -0700

    spotless

--- src/main/java/frc/robot/subsystems/climb/Climb.java
+++ src/main/java/frc/robot/subsystems/climb/Climb.java
@@ -10,20 +10,13 @@ package frc.robot.subsystems.climb;
 import static edu.wpi.first.units.Units.Seconds;
 import static edu.wpi.first.units.Units.Volts;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.ctre.phoenix6.SignalLogger;
 import com.pathplanner.lib.auto.AutoBuilder;
-
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-import frc.robot.limelight.Limelight;
 import frc.robot.limelight.LimelightHelpers;
 import frc.robot.utils.DisableSubsystem;
 import org.littletonrobotics.junction.Logger;
@@ -37,19 +30,21 @@ public class Climb extends DisableSubsystem {
   public Climb(boolean disabled, ClimbIO climbIO) {
     super(disabled);
     this.climbIO = climbIO;
-    m_sysIdRoutine = new SysIdRoutine(
-        new SysIdRoutine.Config(
-            Volts.of(0.2).per(Seconds.of(1)), // Use default ramp rate (1 V/s)
-            Volts.of(6), // Reduce dynamic step voltage to 4 to prevent brownout
-            null, // Use default timeout (10 s)
-            // Log state with Phoenix SignalLogger class
-            (state) -> SignalLogger.writeString("state", state.toString())),
-        new SysIdRoutine.Mechanism(
-            (volts) -> climbIO
-                .getMotor()
-                .setControl(climbIO.getVoltageRequest().withOutput(volts.in(Volts))),
-            null,
-            this));
+    m_sysIdRoutine =
+        new SysIdRoutine(
+            new SysIdRoutine.Config(
+                Volts.of(0.2).per(Seconds.of(1)), // Use default ramp rate (1 V/s)
+                Volts.of(6), // Reduce dynamic step voltage to 4 to prevent brownout
+                null, // Use default timeout (10 s)
+                // Log state with Phoenix SignalLogger class
+                (state) -> SignalLogger.writeString("state", state.toString())),
+            new SysIdRoutine.Mechanism(
+                (volts) ->
+                    climbIO
+                        .getMotor()
+                        .setControl(climbIO.getVoltageRequest().withOutput(volts.in(Volts))),
+                null,
+                this));
   }
 
   @Override
@@ -82,17 +77,20 @@ public class Climb extends DisableSubsystem {
   }
 
   public Command daToNearestStageAprilTag() {
-    return this.defer(() -> {
-      Pose2d botPose2d = LimelightHelpers.getBotPose2d(ClimbConstants.kBotPoseLimelightId);
-      return AutoBuilder.pathfindToPose(
-          botPose2d.nearest(DriverStation.getAlliance().equals(Alliance.Red) ? ClimbConstants.kStagePosesRed
-              : ClimbConstants.kStagePosesBlue),
-          null,
-          0.0, // Goal end velocity in meters/sec
-          0.0 // Rotation delay distance in meters. This is how far the robot should travel
+    return this.defer(
+        () -> {
+          Pose2d botPose2d = LimelightHelpers.getBotPose2d(ClimbConstants.kBotPoseLimelightId);
+          return AutoBuilder.pathfindToPose(
+              botPose2d.nearest(
+                  DriverStation.getAlliance().equals(Alliance.Red)
+                      ? ClimbConstants.kStagePosesRed
+                      : ClimbConstants.kStagePosesBlue),
+              null,
+              0.0, // Goal end velocity in meters/sec
+              0.0 // Rotation delay distance in meters. This is how far the robot should travel
               // before attempting to rotate.
-      );
-    });
+              );
+        });
   }
 
   public Command retractClimber() {
--- src/main/java/frc/robot/subsystems/climb/ClimbConstants.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbConstants.java
@@ -7,67 +7,72 @@
 
 package frc.robot.subsystems.climb;
 
-import java.util.ArrayList;
-
 import com.ctre.phoenix6.configs.*;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import java.util.ArrayList;
 
 public final class ClimbConstants {
 
-        public static final int kLeftClimbMotorID = 18;
-        public static final double gearRatio = 20; // needs to be tuned
+  public static final int kLeftClimbMotorID = 18;
+  public static final double gearRatio = 20; // needs to be tuned
 
-        public static final double kClimbUpPosition = 150 / 20;
+  public static final double kClimbUpPosition = 150 / 20;
 
-        public static final double kClimbDownPosition = 0;
-        public static final double wheelRadius = 1;
+  public static final double kClimbDownPosition = 0;
+  public static final double wheelRadius = 1;
 
-        public static double kCurrentThreshold = 4.5;
+  public static double kCurrentThreshold = 4.5;
 
-        // // What about the tension from the spring?
-        // public static double gyroRollStableThreshold = 1; // 1 degree of error is
-        // tolerated
-        public static double updateFrequency;
-        public static boolean kUseMotionMagic = false;
+  // // What about the tension from the spring?
+  // public static double gyroRollStableThreshold = 1; // 1 degree of error is
+  // tolerated
+  public static double updateFrequency;
+  public static boolean kUseMotionMagic = false;
 
-        public static final TalonFXConfiguration motorConfig = new TalonFXConfiguration()
-                        .withSlot0(new Slot0Configs().withKS(0).withKV(0).withKP(1).withKI(0).withKD(0))
-                        .withMotorOutput(
-                                        new MotorOutputConfigs()
-                                                        .withNeutralMode(NeutralModeValue.Brake)
-                                                        .withInverted(InvertedValue.Clockwise_Positive))
-                        .withMotionMagic(
-                                        new MotionMagicConfigs()
-                                                        .withMotionMagicAcceleration(400)
-                                                        .withMotionMagicCruiseVelocity(100))
-                        .withCurrentLimits(
-                                        new CurrentLimitsConfigs()
-                                                        .withStatorCurrentLimitEnable(true)
-                                                        .withStatorCurrentLimit(60));
+  public static final TalonFXConfiguration motorConfig =
+      new TalonFXConfiguration()
+          .withSlot0(new Slot0Configs().withKS(0).withKV(0).withKP(1).withKI(0).withKD(0))
+          .withMotorOutput(
+              new MotorOutputConfigs()
+                  .withNeutralMode(NeutralModeValue.Brake)
+                  .withInverted(InvertedValue.Clockwise_Positive))
+          .withMotionMagic(
+              new MotionMagicConfigs()
+                  .withMotionMagicAcceleration(400)
+                  .withMotionMagicCruiseVelocity(100))
+          .withCurrentLimits(
+              new CurrentLimitsConfigs()
+                  .withStatorCurrentLimitEnable(true)
+                  .withStatorCurrentLimit(60));
 
-        public static final String kBotPoseLimelightId = "todo";
-        public static final ArrayList<Pose2d> kStagePosesBlue = new ArrayList<>() {
-                {
-                        // Podium
-                        add(new Pose2d(2.657515287399292, 4.105274677276611, new Rotation2d(0)));
-                        // Amp side
-                        add(new Pose2d(5.990447521209717, 6.171302795410156, new Rotation2d(-2.015216124571914)));
-                        // Etc
-                        add(new Pose2d(6.10739278793335, 2.1172094345092773, new Rotation2d(2.118189174278151)));
-                }
-        };
-        public static final ArrayList<Pose2d> kStagePosesRed = new ArrayList<>() {
-                {
-                        // Podium
-                        add(new Pose2d(13.981689453125, 4.105274677276611, new Rotation2d(3.1415926536)));
-                        // Amp side
-                        add(new Pose2d(10.512321472167969, 6.11283016204834, new Rotation2d(-1.0445000982232164)));
-                        // Etc
-                        add(new Pose2d(10.492830276489258, 2.0977187156677246, new Rotation2d(1.0214219124306612)));
-                }
-        };
+  public static final String kBotPoseLimelightId = "todo";
+  public static final ArrayList<Pose2d> kStagePosesBlue =
+      new ArrayList<>() {
+        {
+          // Podium
+          add(new Pose2d(2.657515287399292, 4.105274677276611, new Rotation2d(0)));
+          // Amp side
+          add(new Pose2d(5.990447521209717, 6.171302795410156, new Rotation2d(-2.015216124571914)));
+          // Etc
+          add(new Pose2d(6.10739278793335, 2.1172094345092773, new Rotation2d(2.118189174278151)));
+        }
+      };
+  public static final ArrayList<Pose2d> kStagePosesRed =
+      new ArrayList<>() {
+        {
+          // Podium
+          add(new Pose2d(13.981689453125, 4.105274677276611, new Rotation2d(3.1415926536)));
+          // Amp side
+          add(
+              new Pose2d(
+                  10.512321472167969, 6.11283016204834, new Rotation2d(-1.0445000982232164)));
+          // Etc
+          add(
+              new Pose2d(
+                  10.492830276489258, 2.0977187156677246, new Rotation2d(1.0214219124306612)));
+        }
+      };
 }
